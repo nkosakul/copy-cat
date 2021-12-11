8ac95d5de3ad41b98216714ad306d84670daad91
nkosakul <nkosakul@3pc.de>
2021-12-11 19:54:59 +0100

    feat: add cut support

--- src/extension.ts
+++ src/extension.ts
@@ -2,7 +2,7 @@ import * as vscode from 'vscode';
 import { textEditor, historyLimit } from './utils/utils';
 import LocalStorageService from './helpers/localStorageService';
 
-const saveSelectionToHistory = (history: LocalStorageService): void => {
+const saveSelectionToHistory = (history: LocalStorageService, action: string): void => {
 	if (!textEditor) { return; }
 
 	const document = textEditor.document;
@@ -10,7 +10,7 @@ const saveSelectionToHistory = (history: LocalStorageService): void => {
 	if (!document) { return; }
 
 	// mimic vscode's behavior
-	vscode.commands.executeCommand('editor.action.clipboardCopyAction');
+	vscode.commands.executeCommand(`editor.action.${action}`);
 
 	// get selection and store it in the history
 	textEditor.selections.map(selection => {
@@ -47,11 +47,13 @@ const clearHistory = (history: LocalStorageService): void => history.clear();
 // this method is called when your extension is activated
 export function activate(context: vscode.ExtensionContext): any {
 	const storageManager = new LocalStorageService(context.globalState, historyLimit);
-	const commandCopy = vscode.commands.registerCommand('copy-cat.copy', (): void => saveSelectionToHistory(storageManager));
+	const commandCopy = vscode.commands.registerCommand('copy-cat.copy', (): void => saveSelectionToHistory(storageManager, 'clipboardCopyAction'));
+	const commandCut = vscode.commands.registerCommand('copy-cat.cut', (): void => saveSelectionToHistory(storageManager, 'clipboardCutAction'));
 	const commandshowHistory = vscode.commands.registerCommand('copy-cat.showHistory', (): void => showHistory(storageManager));
 	const commandclearHistory = vscode.commands.registerCommand('copy-cat.clearHistory', (): void => clearHistory(storageManager));
 
 	context.subscriptions.push(commandCopy);
+	context.subscriptions.push(commandCut);
 	context.subscriptions.push(commandshowHistory);
 	context.subscriptions.push(commandclearHistory);
 }
