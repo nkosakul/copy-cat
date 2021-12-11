56e3582ee98b183bb29b251dca1f3eb889910f8b
nkosakul <nkosakul@3pc.de>
2021-12-11 22:06:52 +0100

    fix: prevent prettier from crying

--- src/extension.ts
+++ src/extension.ts
@@ -94,4 +94,6 @@ export function activate(context: vscode.ExtensionContext) {
 }
 
 // this method is called when your extension is deactivated
-export function deactivate() { }
+export function deactivate() {
+  // do nothing
+}
