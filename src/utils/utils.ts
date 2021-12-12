0b59e965e6a9c93a168a4befb4489595a0f8be07
nkosakul <nkosakul@3pc.de>
2021-12-12 15:18:47 +0100

    feat: set default limit to 15

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -2,7 +2,7 @@ import * as vscode from 'vscode';
 
 export const historyLimit: number = vscode.workspace
   .getConfiguration('copy-cat')
-  .get('limit', 4);
+  .get('limit', 15);
 
 export const persistHistory: boolean = vscode.workspace
   .getConfiguration('copy-cat')
